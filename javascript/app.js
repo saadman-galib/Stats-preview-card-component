97b723ed81cd43dcbaefde74cf3390969005410a
Md. Saadman Galib Rabbi <galib2829@gmail.com>
2021-10-01 22:39:25 +0600

    Updating files

--- javascript/app.js
+++ javascript/app.js
@@ -2,12 +2,9 @@ var mediaQuery = window.matchMedia('(max-width: 1160px)')
 var image01 = document.querySelector('.right-part-img')
 
 if (mediaQuery.matches){
-    // alert("Ok")
-    //document.image01.src('./images/image-header-mobile.jpg');
     image01.src = ('./images/image-header-mobile.jpg');
 }
 
 else{
-    //document.image01.src('./images/image-header-desktop.jpg');
     image01.src = ('./images/image-header-desktop.jpg');    
 }
